bdb293433d38e9163df326c1af5eeb12a5c7ded6
Gabriel Lazaro <148295634+621365@users.noreply.github.com>
2024-01-25 10:58:23 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
-echo "Hello World! ";
+<!doctype html>
+<html>
+  <head>
+    <title>
+      PHP Test
+    </title>
+    <meta property="og:description description" content="Untitled Web Application Project">
+    <script src="./libext.js"></script>
+    <link rel="stylesheet" type="text/css" href="./style.css">
+  </head>
+  <body>
+    <h2>PHP Test</h2>
+    <?php
+    echo "Hello World! If you see this, the test worked.";
+    ?>
+    <p>If you do not see a message confirming this test worked, this test failed.</p>
+  </body>
+</html>
