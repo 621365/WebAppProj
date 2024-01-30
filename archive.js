e32e7d806aca5398be3963e49b18c2eb9fe40a26
Gabriel Lazaro <148295634+621365@users.noreply.github.com>
2024-01-30 11:03:45 -0800

    Create archive.js

---
+++ archive.js
@@ -0,0 +1,25 @@
+// Because there is no backend, all articles must be hard-coded into this file. 
+var archive = {
+  {
+    "title": "Test Article",
+    "content": "This is a test article. This holds the template for which all articles will be made.",
+    "meta": {
+      "description": "This is a test article.",
+      "author": "Gabriel Lazaro",
+      "tags": ["Test", "Placeholder"],
+      "image": {
+        "source": null,
+        "alt": "No Image Shown"
+      },
+      "timestamp": {
+          "year": 2024,
+          "month": 1,
+          "day": 30,
+          "hour": 10,
+          "minute": 59,
+          "second": 3.14159,
+          "UTC": -8
+        }
+    }
+  } 
+}
