e5d75205d720f7c99229e36e75b42428858dfa8f
Gabriel Lazaro <148295634+621365@users.noreply.github.com>
2024-02-01 10:37:00 -0800

    Update archive.js

--- archive.js
+++ archive.js
@@ -1,5 +1,5 @@
 // Because there is no backend, all articles must be hard-coded into this file. 
-var archive = {
+var archive = [
   {
     "title": "Test Article",
     "content": "This is a test article. This holds the template for which all articles will be made.",
@@ -22,4 +22,42 @@ var archive = {
         }
     }
   } 
+]
+
+// Search Retrieval Framework
+
+// Aquires the Hard-Coded ID of the 
+function retrieve(title) {
+  //Initialize
+  let results = [{
+    "title": "Duplicate Articles",
+    "content": "There is more than one article with the name that you are searching for. Please select one of the articles below to view it.<br><br>",
+    "meta": {
+      "description": "This is a test article.",
+      "author": "Gabriel Lazaro",
+      "tags": ["Error", "Duplicate"],
+      "image": {
+        "source": null,
+        "alt": "No Image Shown"
+      },
+      "timestamp": {
+          "year": 1987,
+          "month": 11,
+          "day": 13,
+          "hour": 3,
+          "minute": 13,
+          "second": 6,
+          "UTC": 0
+        }
+    }
+  }];
+
+  // Search
+  let j = 1;
+  for (var i = 0; I < archive.length; i++) {
+    if (article[i].title == title) { results[j] = article[i]; j++; }
+  }
+
+  // Return Based on Duplicates
+  return (results.length > 2) ? results : results[1];
 }
