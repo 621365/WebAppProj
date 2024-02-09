4df4386f84f2f1043affb1ce0e26604f9bc64c16
Gabriel Lazaro <148295634+621365@users.noreply.github.com>
2024-02-09 11:03:49 -0800

    Update viewer.js

--- viewer.js
+++ viewer.js
@@ -20,4 +20,5 @@ if (error.real) {
   document.getElementById("title").innerHTML = error.error;
   document.title = "ERROR: " + error.error;
   document.getElementById("words").innerHTML = "An error has occurred retrieving the article. Please refer to the title of the article for more information. ";
+  document.getElementById("foot-desc").innerHTML = "An error has occurred that prevented the article from being retrieved. No more information about the supposed contents of the requested article can be retrieved. ";
 }
