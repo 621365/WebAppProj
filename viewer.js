1f0fb4f63595ff60ebcc9275c3a1fedc5e1b3059
Gabriel Lazaro <148295634+621365@users.noreply.github.com>
2024-02-09 10:53:42 -0800

    Update viewer.js

--- viewer.js
+++ viewer.js
@@ -1,8 +1,17 @@
 // Code Specifically for the Article Viewer
+var data;
+var error = {"error": "", "real":false};
 if (GET("id").length) {
-  
+  for (var i = 0; I < archive.length; i++) {
+    if (archive[i].meta.uuid == GET("id")) {
+      data = archive[i];
+    }
+    if (typeof data == "undefined") {error = true; error.error = "Nonexistent Article";}
+  }
 } else if (GET("title").length) {
-
+  data = retrieve(GET("title"));
+  if (data.meta.error) {error.real = true; error.error = "No Such Article Title";} else if (data.length > 1) error.error = "Duplicates";
 } else {
-
+  error.real = true;
+  error.error = "No Article Requested for Viewing";
 }
