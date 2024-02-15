46a71165e7f3660afd011c16a0639ec7eb797945
Gabriel Lazaro <148295634+621365@users.noreply.github.com>
2024-02-15 11:24:36 -0800

    Update search.js

--- search.js
+++ search.js
@@ -1,12 +1,14 @@
 // FrameWork for Search Engine. Parts of this code may be used by other parts of this site since it also contains the code to contain articles in "cards."
 
 // Placeholder Archive Card (Assumes that In UL)
-function listCard(title, description, meta) {
+function listCard(title, description, meta, id) {
  return `
  <li>
-   <div class="card-title">`+title+`</div>
-   <div class="card-descr">`+description+`</div>
-   <div class="card-metas">`+JSON.strigify(meta)+`</div>
+   <a href="./viewer.html?uuid=`id`">
+    <div class="card-title">`+title+`</div>
+    <div class="card-descr">`+description+`</div>
+    <div class="card-metas">`+JSON.strigify(meta)+`</div>
+   </a>
  </li>
  `;
 }
