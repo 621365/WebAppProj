6a2cfb928a5b3ec6ab5e5b3d88dc899ff43b396d
Gabriel Lazaro <148295634+621365@users.noreply.github.com>
2024-02-15 11:19:06 -0800

    Create search.js

---
+++ search.js
@@ -0,0 +1,12 @@
+// FrameWork for Search Engine. Parts of this code may be used by other parts of this site since it also contains the code to contain articles in "cards."
+
+// Placeholder Archive Card (Assumes that In UL)
+function listCard(title, description, meta) {
+ return `
+ <li>
+   <div class="card-title">`+title+`</div>
+   <div class="card-descr">`+description+`</div>
+   <div class="card-metas">`+JSON.strigify(meta)+`</div>
+ </li>
+ `;
+}
