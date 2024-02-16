4eeb7849d7f17c8a4d432eea5c4963ad6a7a939b
Gabriel Lazaro <148295634+621365@users.noreply.github.com>
2024-02-16 11:23:25 -0800

    Update viewer.js

--- viewer.js
+++ viewer.js
@@ -22,6 +22,7 @@ function media(type = null, subtype = null, source = null, alt = "Unknown or und
         case "MUTE":
           break;
         case "SHOK":
+          encode = `<embed id="mediaFile" src="` + source + `" quality="high">Because Shockwave has been killed by Adobe, this file may not work. If you truly want to view this file, please use the Ruffle emulator to play this file.</embed>`;
           break;
         case "AIDS":
           break;
