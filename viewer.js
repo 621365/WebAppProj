6ae19f19eb4f8e10dd3dd52ec2365f79d6cbcec5
Gabriel Lazaro <148295634+621365@users.noreply.github.com>
2024-02-16 09:22:43 -0800

    Update viewer.js

--- viewer.js
+++ viewer.js
@@ -43,7 +43,7 @@ if (error.real) {
       document.getElementById("meta").innerHTML = "An irregular has occurred that prevented this page from promptly showing your desired article. This is not an error, but is not the normal behavior expected of this site. More than one article share the same title as the one that you are wanting to view. The articles that you might want to access will be shown alongside their metadata; please choose which article to view. If you would like to prevent this error from occurring again, please refer to articles through their UUID instead of through their title. ";
       document.getElementById("words").innerHTML = "<ul>";
       for (var k = 0; k < data.length; k++) {
-        document.getElementById("words").innerHTML += listCard(data[k + 1].title, data[k + 1].content, data[k + 1].meta, data[k + 1].meta.uuid);
+        document.getElementById("words").innerHTML += listCard(data[k].title, data[k].content, data[k].meta, data[k].meta.uuid);
       }
       document.getElementById("words").innerHTML += "</ul>";
     }
