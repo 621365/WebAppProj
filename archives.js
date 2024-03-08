c3470152ef36b8804fe722220380c2fc10f713f5
Gabriel Lazaro <148295634+621365@users.noreply.github.com>
2024-03-08 10:55:53 -0800

    Update archives.js

--- archives.js
+++ archives.js
@@ -3,3 +3,11 @@ function timewarp(time) {
   //limit: 2038293, 1905350
   
 }
+
+// Function Intended for Exclusive Use With Archive List
+function determineUTC() {
+  let uUTC = userUTC();
+  alert("Your UTC Dialation in your location is " + uUTC.toString() + ".");
+  document.getElementsByName("utc")[0].value = uUTC;
+  return uUTC;
+}
