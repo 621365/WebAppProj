ccc3b95bf1cf015520e69d5aee50536b1b56595e
Gabriel Lazaro <148295634+621365@users.noreply.github.com>
2024-03-14 11:10:19 -0700

    Update archives.js

--- archives.js
+++ archives.js
@@ -1,7 +1,7 @@
 // Goes to File Closest to Specified Timestamp
 function timewarp(time) {
   //limit: 2038293, 1905350
-  if (time > 2038293 || time < 1905350) {
+  if (((time < 0) ? (time * -1) : time) > 2147483647) {
     console.error("Uncaught RangeError: " + time.toString() + "is not within the acceptable time range");
     return -1;
   } else {
