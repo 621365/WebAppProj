abfc89f771ade1f593afeff30c735e3fb4e3ba20
Gabriel Lazaro <148295634+621365@users.noreply.github.com>
2024-04-19 11:21:54 -0700

    attempt at offline compatibility

---
+++ liboff.js
@@ -0,0 +1,43 @@
+// An attempt to make the site work even without internet.
+// Funny thing, because the entire DataBase is in a JSON, I could make (almost) all of the site work without Internet, I just need to place a warning in the footer that the user is offline. 
+const cacheBar = "cacheBar";
+const bar = [
+  "/WebAppProj",
+  "index.html",
+  "style.css",
+  "libext.js",
+  "libret.js",
+  "archive.js",
+  "articles.js",
+  "archives.html",
+  "archives.js",
+  "contact.html",
+  "hot.html",
+  "index.html",
+  "info.html",
+  "legal.html",
+  "privacy.html",
+  "search.html",
+  "search.js",
+  "submit.html",
+  "terms.html",
+  "viewer.html",
+  "viewer.js",
+  "favicon.svg"
+];
+
+self.addEventListener("install", function(e) {
+  e.waitUntil(
+    caches.open(cacheBar).then(function(cache) {
+      return cache.addAll(bar)
+    })
+  )
+})
+
+self.addEventListener("fetch", function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request)
+    })
+  )
+})
