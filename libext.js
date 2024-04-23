d11716034073857386d8403ad39f4a2f9c70fa32
Gabriel Lazaro <148295634+621365@users.noreply.github.com>
2024-04-23 10:52:11 -0700

    added clean cachebar

--- libext.js
+++ libext.js
@@ -192,3 +192,14 @@ function getCookie(cname) {
     }
     return "";
 }
+
+// Reset Cache Bar
+function cleanCacheBar() {
+    if (window.navigator && navigator.serviceWorker) {
+        navigator.serviceWorker.getRegistrations().then(function(registrations) {
+            for(let registration of registrations) {
+              registration.unregister();
+            }
+        });
+    }
+}
