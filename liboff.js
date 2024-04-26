71a663cc908e630a432fa670140ed023be775183
Gabriel Lazaro <148295634+621365@users.noreply.github.com>
2024-04-26 16:52:15 -0700

    catching errors

--- liboff.js
+++ liboff.js
@@ -33,7 +33,7 @@ self.addEventListener("install", function(e) {
     caches.open(cacheBar).then(function(cache) {
 
       // Clear Old CacheBar if Exists
-      for (stuff of bar) if (cache.match(stuff).then(function(result) { return result.ok; })) {
+      for (stuff of bar) if (cache.match(stuff).then(function(result) { try { return result.ok; } catch (e) { return false; } })) {
         cache.delete(stuff);
       }
       
