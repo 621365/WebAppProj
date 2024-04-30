eacc9c00bd24cf698b27e16c3e9e8cf0378a5e4a
Gabriel Lazaro <148295634+621365@users.noreply.github.com>
2024-04-30 10:43:47 -0700

    consistent with alert system now

--- archives.js
+++ archives.js
@@ -3,6 +3,7 @@ function timewarp(time) {
   //limit: 2038293, 1905350
   if (((time < 0) ? (time * -1) : time) > 2147483647) {
     console.error("Uncaught RangeError: " + time.toString() + " is not within the acceptable time range");
+    new alert("Search Error", time.toString() + " is not within the acceptable time range.");
     return -1;
   } else {
     // Prepare for closeTo Function
@@ -19,7 +20,7 @@ function timewarp(time) {
 // Function Intended for Exclusive Use With Archive List
 function determineUTC() {
   let uUTC = userUTC();
-  alert("Your UTC Dialation in your location is " + uUTC.toString() + ".");
+  new alert("UTC Dialation", "Your UTC Dialation in your location is " + uUTC.toString() + ".");
   document.getElementsByName("utc")[0].value = uUTC;
   return uUTC;
 }
