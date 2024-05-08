3448cd0f3282e7c4976bd7c8c05a8ebca3c76ac4
Gabriel Lazaro <148295634+621365@users.noreply.github.com>
2024-05-07 20:31:14 -0700

    attempt at external fix 2

--- liboff.js
+++ liboff.js
@@ -61,7 +61,7 @@ self.addEventListener("fetch", async function(e) {
           // fix for offline?
           return (fetch(e.request) || fetch(response.url));
         }
-        if (!(response instanceof Response)) return (fetch(e.request) || fetch(response.url)); else if (result instanceof Response) return response; else return (fetch(e.request));
+        if (!(response instanceof Response)) return (fetch(e.request)); else if (result instanceof Response) return response; else return (fetch(e.request));
       }
       
       
@@ -72,12 +72,12 @@ self.addEventListener("fetch", async function(e) {
                 if (result.ok) return result; 
                 else {
                     try {
-                        if (response == undefined || !(response instanceof Response)) throw new Error(""); if (response.url.slice(0, ("https://621365.github.io/WebAppProj/").length) == "https://621365.github.io/WebAppProj/") return response; else return (fetch(e.request) || fetch(response.url));
+                        if (response == undefined || !(response instanceof Response)) throw new Error(""); if (response.url.slice(0, ("https://621365.github.io/WebAppProj/").length) == "https://621365.github.io/WebAppProj/") return response; else return (fetch(e.request));
                       } catch (e) {
                         // fix for offline?
-                        return (fetch(e.request) || fetch(response.url));
+                        return (fetch(e.request));
                       }
-                      if (!(response instanceof Response)) return (fetch(e.request) || fetch(response.url)); else if (result instanceof Response) return response; else return (fetch(e.request) || fetch(response.url));
+                      if (!(response instanceof Response)) return (fetch(e.request)); else if (result instanceof Response) return response; else return (fetch(e.request));
                 }
             });
         }); 
