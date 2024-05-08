74adf3dd6dff2d13c05d3d2791bde0de56527eb0
Gabriel Lazaro <148295634+621365@users.noreply.github.com>
2024-05-07 20:52:52 -0700

    domain check

--- liboff.js
+++ liboff.js
@@ -51,6 +51,8 @@ self.addEventListener("fetch", async function(e) {
   e.respondWith(
     // Search Cachebar
  caches.match(e.request, {"ignoreSearch": true}).then(async function(response) {
+
+     if (new URL(e.request).hostname !== window.location.hostname) return fetch(e.request);
       // Caching 2.0 (Dynamic Cache Use)
       try { // Try and not use cachebar
         let result; let i = await (await fetch(e.request).then((r) => { if (r.ok) {result = r; return r;}}) || await fetch(response.url).then((r) => { if (r.ok) {result = r; return r;}})); if (result.ok) return result; else throw new Error("");
